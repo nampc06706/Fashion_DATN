de92ec729adb13f792c5494de8977469e2204bae
hung dinh <so5so6vaso9@gmail.com>
2024-11-07 18:30:34 +0700

    fix bug layout

--- DATN-Theme-main/src/components/Helpers/Cards/ProductCardStyleOneAllProducts.jsx
+++ DATN-Theme-main/src/components/Helpers/Cards/ProductCardStyleOneAllProducts.jsx
@@ -28,7 +28,7 @@ export default function ProductCardStyleOne({ data = {}, type = 1 }) {
 
   return (
     <div
-      className="product-card-one w-full h-full bg-white relative group overflow-hidden"
+      className="product-card-one w-full group"
       style={{ boxShadow: "0px 15px 64px 0px rgba(0, 0, 0, 0.05)" }}
     >
       <div
@@ -48,7 +48,7 @@ export default function ProductCardStyleOne({ data = {}, type = 1 }) {
       </div>
 
       <div className="product-card-details px-[30px] pb-[30px] relative">
-        <div className="absolute w-full h-10 px-[30px] left-0 top-32 group-hover:top-16 transition-all duration-300 ease-in-out">
+        <div className="absolute w-full h-10 px-[30px] left-0 top-32  transition-all duration-300 ease-in-out">
           <button
             type="button"
             className={type === 3 ? "blue-btn w-full" : "yellow-btn w-full"}
